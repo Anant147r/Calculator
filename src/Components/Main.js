bdd042dc84cf20f0cd15e3e9846387948320e789
Anant Rawat <anant.rawat@sunfox.in>
2021-06-11 16:59:46 +0530

    Adding task funtionality

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,21 +2,31 @@ import React, { useState } from "react";
 // import Calculator from "./Calculator";
 import "./Main.css";
 const Main = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([{ id: "3", content: "sdfasdfsadasd" }]);
   const [id, setId] = useState(0);
+  const [textContent, setTextContent] = useState("");
+  const submitHandler = (event) => {
+    event.preventDefault();
+    var obj = { id: id, content: textContent };
+    setItems([...items, obj]);
+    setId(id + 1);
+    setTextContent("");
+  };
+  const changeHandler = (event) => {
+    setTextContent(event.target.value);
+  };
   return (
     <div className="boundary" id="boundary">
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          onChange={(event) => changeHandler(event)}
+          value={textContent}
+        />
+        <button type="submit">Add</button>
+      </form>
+
       <div>
-        <button
-          onClick={(event) => {
-            var obj = { id: id, content: `I am ${id}` };
-            setItems([...items, obj]);
-            setId(id + 1);
-          }}
-        >
-          {" "}
-          Add
-        </button>
         {items.map((ele, index) => {
           return <div key={index}>{ele.content}</div>;
         })}
