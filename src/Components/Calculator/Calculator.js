6898e9b39aca5c8534e0e69d39158de35a95bc38
Anant Rawat <anant.rawat@sunfox.in>
2021-06-22 14:28:50 +0530

    Warnings removed

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import fire from "../../fire";
+// import fire from "../../fire";
 const Calculator = () => {
   const [items, setItemList] = useState([]);
   const [item, setItem] = useState("");
